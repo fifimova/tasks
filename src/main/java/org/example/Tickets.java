21bd127087858ab0deb220a4e4cc38c5dd0a49b1
fifimova <fi_fimova@mail.ru>
2023-09-30 15:31:29 +0300

    task 1

--- src/main/java/org/example/Tickets.java
+++ src/main/java/org/example/Tickets.java
@@ -3,17 +3,15 @@ package org.example;
 import java.util.Arrays;
 
 public class Tickets {
-
-    /* Получаем на входе массив чисел.
-     * Все четные числа увеличиваем на единицу.
-     * Возвращаем кусок списка с 3-го по 7-й элемент. */
+    public static int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static String[] strings = {"Sasha", "Dasha", "Pavel", "Roman", "Dima", "Aleksey"};
 
     public static void main(String[] args) {
-        int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        System.out.println(Arrays.toString(changeArray(ints)));
 
     }
 
+    /* Все четные числа увеличиваем на единицу.
+     * Возвращаем кусок списка с 3-го по 7-й элемент. */
     public static int[] changeArray(int[] ints) {
         for (int i = 0; i < ints.length; i++) {
             if (ints[i] % 2 == 0) {
@@ -21,5 +19,10 @@ public class Tickets {
             }
         }
         return Arrays.copyOfRange(ints, 2, 7);
+    }
+
+    /*Дан List ‹String› names. Удалите первую букву из каждого имени и верните отсортированный список.*/
+    public static String[] task1(String[] str) {
+        Arrays.stream(str).map()
     }
 }
